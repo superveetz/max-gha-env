4bb9922657928583fe0c5a4cd6563e502cf2fafe
Alex Di Vito <alexdiv87@hotmail.com>
2024-08-16 13:42:41 -0700

    test w comment

--- data/database.js
+++ data/database.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-
+// add comment
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
